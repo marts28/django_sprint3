097425779200e51dbaf05738e225cdb3ad009d7b
Danil <dam53@tpu.ru>
2024-10-26 18:59:56 +0700

    Commit3

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -12,8 +12,8 @@ class Category(models.Model):
     slug = models.SlugField(
         "Идентификатор",
         unique=True,
-        help_text="Идентификатор страницы для URL; разрешены символы латиницы, "
-                  "цифры, дефис и подчёркивание."
+        help_text="Идентификатор страницы для URL; разрешены символы"
+                  " латиницы, цифры, дефис и подчёркивание."
     )
     is_published = models.BooleanField(
         "Опубликовано",
